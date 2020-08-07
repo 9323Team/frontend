0bd0b5fff597bb51e7dd92d89ad81da56c9ba221
Jubi <39979025+jubi33717f@users.noreply.github.com>
2020-08-07 18:51:36 +1000

    Update home.js

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -14,26 +14,7 @@ import { connect } from "react-redux";
 
 class Home extends PureComponent{
     state={
-        postsExample:[
-            {tag:'News',photo:'http://img4.a0bi.com/upload/ttq/20190115/1547565135450.jpeg?imageView2/0/w/600/h/800',content:'Today\'s covid19 update',poster:'Admin'},
-            {tag:'Disccuss',photo:'http://img.qqzhi.com/uploads/2018-11-30/073745358.jpg',content:'New advises',poster:'Expert advisor'},
-            {tag:'News',photo:'https://pbs.twimg.com/profile_images/1036730403514736650/PCRxFiEt_400x400.jpg',content:'News here!',poster:'Student'},
-            {tag:'News',photo:'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',content:'Daily briefing',poster:'Student'},
-            {tag:'Projects',photo:'https://m.96weixin.com/Upload/20190813/1565664062266270.jpg',content:'Today\'s national figures',poster:'Admin'},
-            {tag:'Activities',photo:'http://m.imeitou.com/uploads/allimg/2018112616/0duynksoxpy.jpg',content:'Online Meeting of covid 19',poster:'Expert advisor'},
-            {tag:'Qustion(Q&A)',photo:'http://m.imeitou.com/uploads/allimg/2018112616/l2lu0xxcmai.jpg',content:'What are the numbers in brackets?',poster:'Student'},
-            {tag:'News',photo:'http://m.imeitou.com/uploads/allimg/2018092512/4plf33op4i3.jpg',content:'Highlight charts',poster:'Expert advisor'},
-            {tag:'News',photo:'http://m.imeitou.com/uploads/allimg/2018112616/5uv0zdjau20.jpg',content:'Currently a large number of cases ',poster:'Student'},
-            {tag:'Projects',photo:'https://i.redd.it/i8gcpj6gfig11.png',content:'Victorian epicurves ',poster:'Expert advisor'},
-            {tag:'Activities',photo:'http://m.imeitou.com/uploads/allimg/2018092512/p3jlvqgvem3.jpg',content:'Case breakdowns',poster:'Admin'},
-            {tag:'Qustion(Q&A)',photo:'http://m.imeitou.com/uploads/allimg/2018112616/fa2blcywepf.jpg',content:'How can I get a test?',poster:'Student'},
-            {tag:'News',photo:'http://www.ghost64.com/qqtupian/zixunImg/local/2018/09/04/15360417389636.jpeg',content:'State and territory breakdowns ',poster:'Admin'},
-            {tag:'Disccuss',photo:'https://img2.woyaogexing.com/2019/08/06/54e69736b55a41bba256f024ef4c88c2!400x400.webp',content:'Recoveries',poster:'Expert advisor'},
-            {tag:'News',photo:'https://img2.woyaogexing.com/2019/08/06/986a9ea48e4d4d368625284e081400fd!400x400.webp',content:'When does an active case become a recovery?',poster:'Expert advisor'},
-            {tag:'News',photo:'https://img2.woyaogexing.com/2019/08/06/668446d0e2df42a494f84f597a168d5e!400x400.webp',content:'daily cases versus reported ',poster:'Student'},
-            {tag:'Projects',photo:'http://www.ghost64.com/qqtupian/zixunImg/local/2018/09/04/153604173826.jpeg',content:'New find!',poster:'Expert advisor'},       
-            {tag:'Qustion(Q&A)',photo:'http://www.ghost64.com/qqtupian/zixunImg/local/2018/09/04/15360417395968.jpeg',content:'Timeline',poster:'Student'},
-        ],
+        
         dialogExample:[
             {role:'User',photo:'🎃',content:'Hello!'},
             {role:'Uni',photo:'🤖',content:'Hi! How can I help you?'},
